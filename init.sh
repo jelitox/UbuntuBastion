643e4326e6872060e2c3c0f90c762cb3690d3b32
Alexis Sanchez <aasanchez@gmail.com>
2015-04-27 16:03:00 -0430

    Update

--- init.sh
+++ init.sh
@@ -8,10 +8,10 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-#apt-get update
-#apt-get install -qq curl unzip gitcat u -core ack-grep software-properties-common build-essential
+apt-get update
+apt-get install -qq curl unzip gitcat u -core ack-grep software-properties-common build-essential
 
-#git clone https://github.com/aasanchez/WebPro.git Provision
+git clone https://github.com/aasanchez/WebPro.git Provision
 
 if [ -e Provision/hooks/01-configure-apt.sh ]; then
     for f in $(find Provision/hooks/* -maxdepth 1 -executable -type f ! -iname "*.md" ! -iname ".*" | sort --numeric-sort); do
