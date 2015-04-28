2d5fd69fb03fe628da6830dc5789beeab53e9afc
Alexis Sánchez <aasanchez@gmail.com>
2015-04-27 21:01:58 -0430

    Eliminar filtros

--- init.sh
+++ init.sh
@@ -19,12 +19,7 @@ apt-get install -qq curl unzip git-core ack-grep software-properties-common buil
 
 git clone https://github.com/aasanchez/WebPro.git Provision
 
-if [ -e Provision/hooks/01-configure-apt.sh ]; then
-    for f in $(find Provision/hooks/* -maxdepth 1 -executable -type f ! -iname "*.md" ! -iname ".*" | sort --numeric-sort); do
-        . $f
-    done
-else
-    for f in $(find hooks/* -maxdepth 1 -executable -type f ! -iname "*.md" ! -iname ".*" | sort --numeric-sort); do
-        . $f
-    done
-fi
+for f in $(find Provision/hooks/* -maxdepth 1 | sort --numeric-sort); do
+    . $f
+done
+
