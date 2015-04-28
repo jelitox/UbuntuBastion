75fd9e2ea9b9f276e6bf108239c01d97b938e3aa
Alexis Sánchez <aasanchez@gmail.com>
2015-04-27 20:50:43 -0430

    Mejoras

--- hooks/01-configure-base.sh
+++ hooks/01-configure-base.sh
@@ -4,7 +4,11 @@
 # Configuracion de HostName
 #
 echo "Setting Timezone & Locale to UTC & C.UTF-8"
+
 sudo ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 sudo locale-gen C.UTF-8
 export LANG=C.UTF-8
+
 echo "export LANG=C.UTF-8" >> /home/$USER/.bashrc
+apt-get update
+apt-get install -qq curl unzip git-core ack-grep software-properties-common build-essential
--- hooks/02-configure-apt.sh
+++ hooks/02-configure-apt.sh
@@ -4,7 +4,12 @@
 # Configuramos los Sources
 #
 
-# sudo rm /etc/apt/sources.list
-# sudo curl --silent -L https://gist.githubusercontent.com/aasanchez/0f3c626f93aff960b955/raw/ab6c5c6e361f7dd1afd13c2b25a376d469f23bd2/sources.list > sources.list
-# sudo mv sources.list /etc/apt/sources.list
-# sudo apt-get update && sudo apt-get -qq upgrade
+rm /etc/apt/sources.list
+touch /etc/apt/sources.list
+echo "deb $REPO trusty main universe multiverse" >> /etc/apt/sources.list
+echo "deb $REPO trusty-updates main universe multiverse" >> /etc/apt/sources.list
+echo "deb $REPO trusty-backports main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://security.ubuntu.com/ubuntu trusty-security main universe multiverse" >> /etc/apt/sources.list
+echo "deb http://archive.canonical.com/ubuntu trusty partner" >> /etc/apt/sources.list
+apt-get update
+apt-get update && apt-get -qq upgrade
