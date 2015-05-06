9e41ce60a303fc753e495fea0530893ee2a2ecd6
Alexis Sanchez <aasanchez@gmail.com>
2015-05-06 17:01:13 -0430

    Chagne site for hostname

--- hooks/12-install-ngnix.sh
+++ hooks/12-install-ngnix.sh
@@ -3,17 +3,15 @@ echo "\033[32;40;7m Instalacion Nginx\033[0m"
 add-apt-repository -y ppa:nginx/stable
 apt-get update
 apt-get install -qq nginx
-sudo mkdir -p /var/www/$SITE
+sudo mkdir -p /var/www/$HOSTNAME
 sed -i "s/user www-data;/user $USER;/" /etc/nginx/nginx.con
 sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf
 
-
 usermod -a -G www-data $USER
-sudo rm -rf /var/www/html
-sudo mkdir -p /var/www
-sudo chown -R $USER:$USER /var/www
+sudo mkdir -p /var/www/$HOSTNAME
+sudo chown -R $USER:$USER /var/www/$HOSTNAME
 sudo chmod -R 755 /var/www
 
-if [[ ! -z $REPO ]]; then
-    git clone $REPO /var/www
+if [ ! -z $REPO ]; then
+    git clone $REPO /var/www/$HOSTNAME
 fi
