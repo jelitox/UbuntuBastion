0c2580a6ea6e37cb79d602f57a6a173b5433b484
Alexis Sanchez <aasanchez@gmail.com>
2015-05-07 16:40:20 -0430

    Update

--- hooks/13.install-project.sh
+++ hooks/13.install-project.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "\033[32;40;7m Instalacion Proyecto\033[0m"
 delay 2
-echo "\033[1;31;40m Clonando Repositorio\033[1;31m"
+echo "\033[1;31;40m Clonando Repositorio\033[0m"
 delay 1
 git clone git@gitlab.com:$GITUSER/$PROJECT.git /var/www/$HOSTNAME
-echo "\033[1;31;40m Instalacion Dependencias\033[1;31m"
+echo "\033[1;31;40m Instalacion Dependencias\033[0m"
 delay 1
 cd /var/www/$HOSTNAME
 composer install
