27d1da5bd77ecc704e2d1dc33b2a27013658a642
Alexis Sanchez <aasanchez@gmail.com>
2015-05-08 11:38:05 -0430

    Se agrego cd, para forzar que siempre esta apuntando al home

--- init.sh
+++ init.sh
@@ -19,5 +19,6 @@ apt-get install -qq curl unzip git-core ack-grep software-properties-common buil
 git clone https://github.com/aasanchez/UbuntuBastion.git Provision
 
 for f in $(find Provision/hooks/* -maxdepth 1 | sort --numeric-sort); do
+    cd #Garantizamos que el puntero vuelva a home
     . $f
 done
