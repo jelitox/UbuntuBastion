5f5bff87d9681e92954dd719b8f840656f9ba77b
Alexis Sanchez <aasanchez@gmail.com>
2015-05-08 11:23:46 -0430

    Show repo url

--- hooks/13.install-project.sh
+++ hooks/13.install-project.sh
@@ -3,6 +3,7 @@ echo "\033[32;40;7m Instalacion Proyecto\033[0m"
 sleep 2
 echo "\033[1;31;40m Clonando Repositorio\033[0m"
 sleep 1
+echo "--->git@gitlab.com:$GITUSER/$PROJECT.git"
 git clone git@gitlab.com:$GITUSER/$PROJECT.git /var/www/$HOSTNAME
 echo "\033[1;31;40m Instalacion Dependencias\033[0m"
 sleep 1
