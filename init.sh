441be3cbb84a6d0ab7639bae5791a46d7fd48aec
Alexis Sánchez <alexis@juegaenlinea.com>
2015-07-29 11:13:25 -0430

    Rename

--- init.sh
+++ init.sh
@@ -10,30 +10,54 @@ USUARIO='vagrant'
 HOSTNAME='bastion'
 REPO='http://us-east-1.ec2.archive.ubuntu.com/ubuntu/' # http://us-east-1.ec2.archive.ubuntu.com/ubuntu/
 
-NR_LICENCE= #New Relic Licence
-
 if [ -f id_rsa ]; then
-    if [ ! -d .ssh ]; then
-        mkdir .ssh
+    if [ ! -d ~/.ssh ]; then
+        mkdir ~/.ssh
     fi
-    rm .ssh/id_rsa
+    rm ~/.ssh/id_rsa
     cp id_rsa ~/.ssh/id_rsa
     chmod 400 ~/.ssh/id_rsa
-else
-    if [ -f deploy_key ]; then
-        rm deploy_key*
-    fi
-    ssh-keygen -t rsa -b 4096 -N "" -C "servers@$HOSTNAME" -f deploy_key
-    echo "Copie el siguiente public key en un sitio seguro"
-    echo "Agrege como deploy key para el proyecto que desea instalar"
-    echo ""
-    echo ""
-    cat deploy_key.pub
-    echo ""
-    echo ""
-    #read -p "Presione Enter para continuar... " -n1 -s
 fi
 
-for hook in $(find hooks.d/* -maxdepth 1 | sort --numeric-sort); do
-    . $hook
-done
+####
+# Base Items
+##########
+
+# Configuracion de Repositorios
+. hooks.d/01-configure-apt
+
+# Paquetes Basicos
+. hooks.d/02-configure-base
+
+# Set Hostname
+. hooks.d/03-configure-hostname
+
+
+####
+# Lenguajes de Programación
+##########
+
+# PHP
+. hooks.d/25-install-php
+
+# Node.js
+. hooks.d/26-install-nodejs
+
+
+####
+# Servidor Web
+##########
+
+# Nginx
+. hooks.d/27-install-nginx
+
+
+####
+# Herramientas de Monitoreo y Analisis
+##########
+
+# New relic
+. hooks.d/80-newrelic
+
+# Loggly
+. hooks.d/81-loggly
